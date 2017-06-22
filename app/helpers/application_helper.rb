8bcd13f3bcfedbb947909de6c0042fd85fb593e2
Jesse Farber <jessefarber@earthlink.net>
2017-06-22 19:53:34 +0200

    Conditional background image logic in main view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def black_bg_pages?(controller_path)
+    if controller_path.include?('artist')
+      true
+    elsif controller_path.include?('admin')
+      true
+    elsif controller_path.include?('developer')
+      true
+    else
+      false
+    end
+  end
 end
