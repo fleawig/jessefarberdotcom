dea7199770b663b304d484295734098df296a2d4
Jesse Farber <jesse.farber@laubwerk.com>
2022-01-08 18:27:20 +0100

    Add redirects for artist namespaced routes to avoid errors on older URLs.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resource :admin_dashboard, only: [:show]
   root 'artist/works#index_two_d'
   get 'developer', to: 'static_pages#developer_portfolio', as: 'developer_portfolio'
-  # Root inside artist namespace displays artist partials and nav with blank
-  # content, since artist controller home action has no content.
   get 'works/2D/:id', to: 'artist/works#show_two_d', as: 'two_d_work'
   get 'works/2D', to: 'artist/works#index_two_d', as: 'two_d_works'
   get 'works/3D/:id', to: 'artist/works#show_three_d', as: 'three_d_work'
@@ -17,7 +15,7 @@ Rails.application.routes.draw do
   get 'RNL', to: 'artist/rnl_issues#index', as: 'rnl'
   get 'CV', to: 'artist/static_pages#cv', as: 'cv'
   get 'news', to: 'artist/news_items#index', as: 'news'
-  # end
+
   namespace :admin do
     resources :works
     resources :book_pages
@@ -26,3 +24,16 @@ Rails.application.routes.draw do
     get 'books/:book_title', to: 'book_pages#show_book', as: 'show_book'
   end
 end
+
+# These routes are obsolete. We use redirects to handle older links gracefully.
+  namespace :artist do
+    get 'works/2D/:id' => redirect('works/2D/%{id}')
+    get 'works/2D' => redirect('works/2D')
+    get 'works/3D/:id' => redirect('works/3D/%{id}')
+    get 'works/3D' => redirect('works/3D')
+    get 'books'=> redirect('books')
+    get 'audio'=> redirect('audio')
+    get 'RNL'=> redirect('RNL')
+    get 'CV'=> redirect('CV')
+    get 'news'=> redirect('news')
+  end
